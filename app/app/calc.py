44681c3f49d5ba67b6da734a1690a495df10ee51
Ayobami <ayobami.t.owolabi@gmail.com>
2023-03-15 01:21:31 +0000

    Codechecked project

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,7 +5,7 @@ Calculator functions
 
 def add(x, y):
     """Add x and y and return result."""
-    return x+y
+    return x + y
 
 
 def subtract(x, y):
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,23 +7,21 @@ from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
-from django.db import connection, OperationalError
 
 
 class Command(BaseCommand):
-    help = 'Django command to wait for database'
+    """Django command to wait for database."""
 
     def handle(self, *args, **options):
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
         db_up = False
-        while not db_up:
+        while db_up is False:
             try:
-                connection.ensure_connection()
+                self.check(databases=['default'])
                 db_up = True
-            except OperationalError as e:
-                self.stdout.write(f'Database unavailable: {str(e)}')
-                self.stdout.write('Waiting 1 second before retrying...')
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
